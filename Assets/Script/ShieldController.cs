833fca9f38ab9f10d4b284777806eedbfbd33acd
Windfinz <Phongdraco@gmail.com>
2024-03-20 17:58:30 +0700

    shield controller

--- Assets/Script/ShieldController.cs
+++ Assets/Script/ShieldController.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShieldController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform shield;
+    private float dis = 1f;
+    public FixedJoystick joystick;
+    public float rotationSpeed = 5f;
+    public float smoothing = 5f; 
+    private Vector3 currentDirection = Vector3.zero;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (joystick.Direction.magnitude > 0)
+        {
+            Vector2 direction = joystick.Direction;
 
+            currentDirection = Vector3.Lerp(currentDirection, direction, smoothing * Time.deltaTime);
+
+            float rads = Mathf.Atan2(currentDirection.y, currentDirection.x);
+            float degrees = rads * Mathf.Rad2Deg;
+
+            shield.localPosition = new Vector3(Mathf.Cos(rads) * dis, Mathf.Sin(rads) * dis, 0);
+            shield.localEulerAngles = new Vector3(0, 0, degrees - 90);
+        }
     }
 }
