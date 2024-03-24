b8fe86035e4568a555abd12c18bdd29d3c7da9ed
Phong <Phongdraco@gmail.com>
2024-03-24 18:53:27 +0700

    health bar fill, DOTWEEN added

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,14 +1,44 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField]
     private float playerHealth;
 
+    [Header("Health")]
+    public Image healthBar;
+    public float currentHealth;
+    private float targetFill;
+    public float fillSpeed;
+
+    private float maxHealth;
+
+    private void Start()
+    {
+        currentHealth = playerHealth;
+        maxHealth = playerHealth;
+        UpdateHealthBar();
+    }
+
+    private void Update()
+    {
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar() 
+    {
+        targetFill = currentHealth / maxHealth;
+        healthBar.DOFillAmount(targetFill, fillSpeed);
+    } 
+        
+
     public void TakeDamage(float dmg)
     {
-        playerHealth -= dmg;
-        if(playerHealth <= 0)
+        currentHealth -= dmg;
+        UpdateHealthBar();
+        if (currentHealth <= 0)
         {
             //Kill method;
         }
